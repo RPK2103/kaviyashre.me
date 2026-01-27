99838be8fdc06852bc064c206ada1b2c7d39bdc4
RPK2103 <kaviyashreragupathy@gmail.com>
2026-01-28 00:45:52 +0530

    initial setup of portfolio project and include all the assets

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+const App = () => {
+    return (
+        <main>
+
+            <div>
+                <h2>Welcome to my 3d portfolio !!</h2>
+            </div>
+        </main>
+    )
+}
+export default App
